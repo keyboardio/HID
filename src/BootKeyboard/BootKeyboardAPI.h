8d2bc7d80d0ecbc49d2a6f2736b97cb56f1338ec
Gergely Nagy <algernon@keyboard.io>
2017-12-01 15:30:17 +0100

    BootKeyboard: Implement a way to set the protocol forcibly
    In rare cases it may be desirable to be able to set the report protocol, to
    force it, and override whatever the host is doing or not doing. The
    `setProtocol` method does just this.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/BootKeyboard/BootKeyboardAPI.h
+++ src/BootKeyboard/BootKeyboardAPI.h
@@ -9,6 +9,7 @@ class BootKeyboardAPI {
   virtual void releaseAll(void) = 0;
   virtual int sendReport(void) = 0;
   virtual uint8_t getProtocol(void) = 0;
+  virtual void setProtocol(uint8_t protocol) = 0;
   virtual boolean isModifierActive(uint8_t k) = 0;
   virtual boolean wasModifierActive(uint8_t k) = 0;
 };
